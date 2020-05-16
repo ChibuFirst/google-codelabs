f6ee9096613f1bddf8427627b68f2ceab2619303
ChibuFirst <chibuzoonuoha57@gmail.com>
2020-05-16 08:43:57 +0100

    Updated the GuessTheWord app for lesson 5.3

--- GuessTheWord/app/src/main/java/com/example/guesstheword/screens/score/ScoreFragment.kt
+++ GuessTheWord/app/src/main/java/com/example/guesstheword/screens/score/ScoreFragment.kt
@@ -25,18 +25,15 @@ class ScoreFragment : Fragment() {
         viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments!!).score)
         viewModel = ViewModelProvider(this, viewModelFactory)
             .get(ScoreViewModel::class.java)
-        // Add observer for score
-        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
-            binding.scoreText.text = newScore.toString()
-        })
+        binding.scoreViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
         viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
             if (playAgain) {
                 findNavController().navigate(ScoreFragmentDirections.actionRestart())
                 viewModel.onPlayAgainComplete()
             }
         })
-
-        binding.playAgainButton.setOnClickListener {  viewModel.onPlayAgain()  }
 
         return binding.root
     }
